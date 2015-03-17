f43864be24d9b6e82a874e4a42f4e30b74e08245
calzoneman <calzoneman@gmail.com>
2015-03-17 00:55:13 -0500

    Fix parseUrl spelling in index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('source-map-support').install();
 
 exports.lookup = require('./lib/lookup');
-exports.parseUrl = require('./lib/parseurl');
+exports.parseUrl = require('./lib/parseUrl');
+exports.Media = require('./lib/media');

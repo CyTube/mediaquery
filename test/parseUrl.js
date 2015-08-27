01eac8997243d678ddbca4143ba41e3fc61e857f
calzoneman <calzoneman@gmail.com>
2015-08-26 23:22:58 -0700

    Clean up broken tests

--- test/parseUrl.js
+++ test/parseUrl.js
@@ -5,22 +5,18 @@ var parseUrl = require('../lib/parseUrl');
 var tests = {
     'https://www.youtube.com/watch?v=000al7ru3ms': {
         id: '000al7ru3ms',
-        kind: 'single',
         type: 'youtube'
     },
     'https://youtube.com/playlist?list=PLVXq77mXV539VYxMIcXeQMOv3Ffo22z5E': {
         id: 'PLVXq77mXV539VYxMIcXeQMOv3Ffo22z5E',
-        kind: 'playlist',
-        type: 'youtube'
+        type: 'youtube-playlist'
     },
     'https://vimeo.com/59859181': {
         id: '59859181',
-        kind: 'single',
         type: 'vimeo'
     },
     'http://www.dailymotion.com/video/x2j9c73_watch-nasa-test-the-largest-most-powerful-rocket-booster-ever-built_travel': {
         id: 'x2j9c73',
-        kind: 'single',
         type: 'dailymotion'
     }
 };

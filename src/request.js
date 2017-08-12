b68f06ece3fbe1056527a7fbfecb36d6b3a4cbbb
Calvin Montgomery <calzoneman@gmail.com>
2017-08-12 15:45:40 -0700

    Refactor request function a bit

--- src/request.js
+++ src/request.js
@@ -1,37 +1,65 @@
-import http from 'http';
-import https from 'https';
-import urlparse from 'url';
+import { get as httpGet } from 'http';
+import { get as httpsGet } from 'https';
+import { parse as urlParse } from 'url';
 import Promise from 'bluebird';
 
-export function request(url, options = {}) {
-    return new Promise(function(resolve, reject) {
-        const parsed = urlparse.parse(url);
-        if (!parsed.protocol || !parsed.protocol.match(/^https?:$/)) {
-            return reject(new Error(`Invalid protocol '${parsed.protocol}'`));
+const DEFAULT_OPTS = {
+    timeout: 30000
+};
+
+export function request(url, opts = {}) {
+    return new Promise((resolve, reject) => {
+        const options = {};
+
+        Object.assign(options, parseURL(url), DEFAULT_OPTS, opts);
+
+        if (!/^https?:$/.test(options.protocol)) {
+            reject(new Error(
+                `Unacceptable protocol "${options.protocol}"`
+            ));
+
+            return;
         }
 
-        const transport = parsed.protocol === 'https:' ? https : http;
-        for (let key in options) {
-            const val = options[key];
-            parsed[key] = val;
-        }
-        const req = transport.get(parsed, res => {
-            let buffer = '';
-            res.on('data', data => buffer += data);
+        // this is fucking stupid
+        const get = options.protocol === 'https:' ? httpsGet : httpGet;
+        const req = get(options);
 
-            return res.on('end', () => {
-                res.data = buffer;
-                return resolve(res);
-            });
+        req.setTimeout(options.timeout, () => {
+            const error = new Error('Request timed out');
+            error.code = 'ETIMEDOUT';
+            reject(error);
         });
 
-        return req.on('error', err => reject(err));
+        req.on('error', error => {
+            reject(error);
+        });
+
+        req.on('response', res => {
+            let buffer = '';
+            res.setEncoding('utf8');
+
+            res.on('data', data => {
+                buffer += data;
+
+                if (options.maxResponseSize && buffer.length > options.maxResponseSize) {
+                    req.abort();
+                    reject(new Error('Response size limit exceeded'));
+                }
+            });
+
+            res.on('end', () => {
+                res.data = buffer;
+                resolve(res);
+            });
+        });
     });
 }
 
 export function getJSON(url, options = {}) {
     return request(url, options).then(function(res) {
         switch (res.statusCode) {
+            // TODO: replace with generic 4xx/5xx detector, remove logic from here
             case 400:
             case 403:
             case 404:
@@ -50,4 +78,15 @@ export function getJSON(url, options = {}) {
             throw new Error('Response could not be decoded as JSON');
         }
     });
+}
+
+function parseURL(urlstring) {
+    const url = urlParse(urlstring);
+
+    // legacy url.parse doesn't check this
+    if (url.protocol == null || url.host == null) {
+        throw new Error(`Invalid URL "${urlstring}"`);
+    }
+
+    return url;
 }
---
+++ test/request.js
@@ -0,0 +1,92 @@
+const http = require('http');
+const assert = require('assert');
+const { request } = require('../lib/request');
+
+describe('request', () => {
+    describe('#request', () => {
+        let server;
+        let serveFunc;
+
+        beforeEach(() => {
+            serveFunc = function (req, res) {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.write('{"success":true}');
+                res.end();
+            };
+
+            server = http.createServer((req, res) => serveFunc(req, res));
+            server.listen(10111);
+        });
+
+        afterEach(done => {
+            server.close(() => done());
+        });
+
+        it('completes a valid request', () => {
+            return request('http://127.0.0.1:10111/').then(res => {
+                assert.strictEqual(res.statusCode, 200);
+                assert.strictEqual(res.data, '{"success":true}');
+            });
+        });
+
+        it('rejects responses when a size limit is set', () => {
+            serveFunc = (req, res) => {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.write(Buffer.alloc(200 * 1024));
+                res.end();
+            };
+
+            return request('http://127.0.0.1:10111/', {
+                maxResponseSize: 100*1024
+            }).then(() => {
+                throw new Error('Expected failure due to response size');
+            }).catch(error => {
+                assert.strictEqual(
+                    error.message,
+                    'Response size limit exceeded'
+                );
+            });
+        });
+
+        it('times out', () => {
+            serveFunc = (req, res) => {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.write('{"success":true}');
+
+                setTimeout(() => res.end(), 100);
+            };
+
+            return request('http://127.0.0.1:10111/', { timeout: 1 }).then(() => {
+                throw new Error('Expected failure due to request timeout');
+            }).catch(error => {
+                assert.strictEqual(
+                    error.message,
+                    'Request timed out'
+                );
+                assert.strictEqual(error.code, 'ETIMEDOUT');
+            });
+        });
+
+        it('rejects URLs with non-http(s) protocols', () => {
+            return request('ftp://127.0.0.1:10111/').then(() => {
+                throw new Error('Expected failure due to unacceptable URL protocol');
+            }).catch(error => {
+                assert.strictEqual(
+                    error.message,
+                    'Unacceptable protocol "ftp:"'
+                );
+            });
+        });
+
+        it('rejects invalid URLs', () => {
+            return request('not valid').then(() => {
+                throw new Error('Expected failure due to invalid URL');
+            }).catch(error => {
+                assert.strictEqual(
+                    error.message,
+                    'Invalid URL "not valid"'
+                );
+            });
+        });
+    });
+});

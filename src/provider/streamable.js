6efd98a91c520d53d2f7052655cee5ff984ebad7
Calvin Montgomery <calzoneman@gmail.com>
2017-09-25 19:17:50 -0700

    Remove direct URLs from streamable
    See calzoneman/sync#706

--- src/provider/streamable.js
+++ src/provider/streamable.js
@@ -4,29 +4,6 @@ import Promise from 'bluebird';
 import { getJSON } from '../request';
 import Media from '../media';
 
-const QUALITIES = [240, 360, 480, 540, 720, 1080, 1440, 2160];
-
-function getQuality(width, height) {
-    let x;
-    if (width > height) {
-        x = height;
-    } else {
-        // Portrait video (why.jpg)
-        x = width;
-    }
-
-    // Streamable doesn't rescale videos, so we kinda have to just guess at the
-    // right quality label for this size
-    for (let q of QUALITIES) {
-        if (x <= q) {
-            return q;
-        }
-    }
-
-    return QUALITIES[QUALITIES.length - 1];
-};
-
-
 export function lookup(id) {
     return getJSON(`https://api.streamable.com/videos/${id}`).then(result => {
         switch (result.status) {
@@ -36,27 +13,10 @@ export function lookup(id) {
         }
 
         let duration = 0;
-        const streams = {};
         for (let key in result.files) {
             const file = result.files[key];
-            let contentType;
-            if (/mp4/.test(key)) {
-                contentType = 'video/mp4';
-            } else if (/webm/.test(key)) {
-                contentType = 'video/webm';
-            } else {
-                continue;
-            }
 
             if (file.status === 2) {
-                const quality = getQuality(file.width, file.height);
-                if (!streams.hasOwnProperty(quality)) {
-                    streams[quality] = [];
-                }
-                streams[quality].push({
-                    link: `https:${file.url}`,
-                    contentType
-                });
                 duration = Math.max(Math.ceil(file.duration), duration);
             }
         }
@@ -71,7 +31,6 @@ export function lookup(id) {
             duration,
             type: 'streamable',
             meta: {
-                direct: streams,
                 thumbnail: `https:${result.thumbnail_url}`
             }
         });

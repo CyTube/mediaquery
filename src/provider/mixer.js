0297d3ae7c5a89c9d2e65e63cf2e542ae071814d
Calvin Montgomery <calzoneman@gmail.com>
2020-08-01 11:28:42 -0700

    Dammit

--- src/provider/mixer.js
+++ src/provider/mixer.js
@@ -1,5 +1,5 @@
 export function setClientID(id) { }
 export function parseUrl(url) { return null; }
 export function lookup(id) {
-    return Promise.reject(new Error('Mixer has shut down as of July 2020"));
+    return Promise.reject(new Error('Mixer has shut down as of July 2020'));
 }

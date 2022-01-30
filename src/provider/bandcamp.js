b88ba2c53fa3992646927d6558d63f2df2e6ef97
Xaekai <ribbon13@gmail.com>
2022-01-30 02:17:08 -0800

    Add direct Bandcamp scraper

--- src/provider/bandcamp.js
+++ src/provider/bandcamp.js
@@ -1,6 +1,10 @@
-import urlparse from 'url';
 import Media from '../media';
 import { ytdl } from '../scraper';
+import { request } from '../request';
+import { findAll } from 'domutils';
+import { parseDom } from '../util/xmldom';
+
+const source = process.env.MQ_BANDCAMP || 'native';
 
 /*
  * Retrieves track data for a Bandcamp song
@@ -8,12 +12,79 @@ import { ytdl } from '../scraper';
  * Returns a Media object
  *
  */
-export function lookup(id) {
-    var [artist, track] = id.split(';');
+export async function lookup(id) {
+    switch(source.toLowerCase()) {
+        case 'native':
+            return _lookupNative(id);
+        case 'external':
+            return _lookupExternal(id);
+        default:
+            throw new Error('Invalid Bandcamp lookup source.');
+    }
+}
+
+async function _lookupNative(id) {
+    const [artist, track] = id.split(';');
     const trackURL = `https://${artist}.bandcamp.com/track/${track}`;
 
-    return ytdl(trackURL).then((info)=>{
+    try {
+        const res = await request(trackURL);
+        if (res.statusCode !== 200) {
+            throw new Error(`Bandcamp lookup failed for ${id}: ${res.statusMessage}`);
+        }
+        const info = {
+            title: '',
+            duration: '',
+            thumbnail: '',
+            url: '',
+        }
+        findAll(elem => elem.name === 'script', parseDom(res.data))
+            .filter(elem => {
+                return elem.attribs.type ==='application/ld+json' || elem.attribs['data-tralbum'];
+            })
+            .forEach(elem => {
+                if(elem.attribs.type ==='application/ld+json'){
+                    // Get text content and parse it
+                    const data = JSON.parse(elem.children[0].data);
+                    info.thumbnail = data.image;
+                } else {
+                    const data = JSON.parse(elem.attribs['data-tralbum']);
+                    const trackinfo = data.trackinfo.pop();
+                    info.title = `${data.artist} - ${trackinfo.title}`;
+                    info.duration = trackinfo.duration;
+                    info.url = trackinfo.file['mp3-128'];
+                }
+            });
 
+        const media = {
+            id: `${artist};${track}`,
+            type: 'bandcamp',
+            title: info.title,
+            duration: Math.floor(info.duration),
+            meta: {
+                direct: {
+                    '480': [{
+                        contentType: 'audio/mp3',
+                        link: info.url,
+                        quality: '480'
+                    }]
+                },
+                thumbnail: info.thumbnail,
+            }
+        };
+
+        return new Media(media);
+    } catch (e) {
+        throw e;
+    }
+}
+
+async function _lookupExternal(id) {
+    const [artist, track] = id.split(';');
+    const trackURL = `https://${artist}.bandcamp.com/track/${track}`;
+
+    try {
+        const info = await ytdl(trackURL);
         const data = {
             id: `${artist};${track}`,
             type: 'bandcamp',
@@ -32,11 +103,10 @@ export function lookup(id) {
         };
 
         return new Media(data);
-    }).catch((err)=>{
-        throw err;
-    });
+    } catch (e) {
+        throw e;
+    }
 }
-
 
 /*
  * Attempts to parse a Bandcamp URL of the forms:
@@ -72,4 +142,3 @@ export function parseUrl(url) {
         type: 'bandcamp'
     };
 }
-

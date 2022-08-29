c1dcf792cd6e9977c04c1e96f23315dad5e3294d
Calvin Montgomery <calzoneman@gmail.com>
2022-08-28 17:04:08 -0700

    Touch up changes

--- src/provider/youtube.js
+++ src/provider/youtube.js
@@ -1,5 +1,6 @@
 import { getJSON, request } from '../request';
 import Media from '../media';
+import { parseDuration } from '../util/isoduration';
 
 import { Counter } from 'prom-client';
 
@@ -7,31 +8,6 @@ const LOGGER = require('@calzoneman/jsli')('mediaquery/youtube');
 
 let API_KEY = null;
 let CACHE = null;
-
-// https://en.wikipedia.org/wiki/ISO_8601#Durations
-const DURATION_SCALE = [
-    [/(\d+)D/, 24*3600],
-    [/(\d+)H/, 3600],
-    [/(\d+)M/, 60],
-    [/(\d+)S/, 1]
-];
-
-/*
- * Parse an ISO 8601 time duration (the format used by YouTube).
- * In the interest of sanity, only days, hours, minutes, and seconds are
- * considered.
- */
-function parseDuration(duration) {
-    let time = 0;
-    for (let [regex, scale] of DURATION_SCALE) {
-        let m;
-        if (m = duration.match(regex)) {
-            time += parseInt(m[1]) * scale;
-        }
-    }
-
-    return time;
-};
 
 const cacheAttemptCount = new Counter({
     name: 'cytube_yt_cache_attempt_count',

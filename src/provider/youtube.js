564d0c4615e80f72722b0f68ac81f837a4c5fc81
Calvin Montgomery <calzoneman@gmail.com>
2023-05-28 21:04:48 -0700

    Refactor YouTube playlists

--- src/provider/youtube.js
+++ src/provider/youtube.js
@@ -5,6 +5,10 @@ import { parseDuration } from '../util/isoduration';
 import { Counter } from 'prom-client';
 
 const LOGGER = require('@calzoneman/jsli')('mediaquery/youtube');
+// Technically the internal CyTube playlist limit is 4000, but even 2000 starts
+// to lag the UI pretty badly and will take at least 40 requests to the YouTube
+// API to load (50 per page).
+const PLAYLIST_ITEM_LIMIT = 2000;
 
 let API_KEY = null;
 let CACHE = null;
@@ -41,6 +45,8 @@ export async function lookup(id) {
         }
     }
 
+    // XXX: consider merging lookupInternal and lookupMany, the only difference
+    // is caching.
     let media = await _lookupInternal(id, cached);
     if (CACHE !== null) {
         try {
@@ -86,6 +92,39 @@ function map403Error(body) {
     }
 }
 
+async function ytRequest(url, headers = {}) {
+    const res = await request(url, { headers });
+    switch (res.statusCode) {
+        case 304:
+            cacheHitCount.inc();
+            return cached;
+        case 400:
+            LOGGER.error('YouTube API returned Bad Request: %s', res.data);
+            throw new Error('Error calling YouTube API: Bad Request');
+        case 403:
+            LOGGER.error('YouTube API returned Forbidden: %s', res.data);
+            throw map403Error(res.data);
+        case 500:
+        case 503:
+            throw new Error('YouTube API is unavailable.  Please try again later.');
+        default:
+            if (res.statusCode !== 200) {
+                throw new Error(`Error calling YouTube API: HTTP ${res.statusCode}`);
+            }
+            break;
+    }
+
+    try {
+        return JSON.parse(res.data);
+    } catch (error) {
+        LOGGER.error(
+            'YouTube API returned non-JSON response: %s',
+            String(res.data).substring(0, 1000)
+        );
+        throw new Error('Error calling YouTube API: could not decode response as JSON');
+    }
+}
+
 function _lookupInternal(id, cached) {
     if (!API_KEY) {
         return Promise.reject(new Error('API key not set for YouTube v3 API'));
@@ -105,126 +144,101 @@ function _lookupInternal(id, cached) {
         };
     }
 
-    return request(url, { headers }).then(res => {
-        switch (res.statusCode) {
-            case 304:
-                cacheHitCount.inc();
-                return cached;
-            case 400:
-                LOGGER.error('YouTube API returned Bad Request: %s', res.data);
-                throw new Error('Error calling YouTube API: Bad Request');
-            case 403:
-                LOGGER.error('YouTube API returned Forbidden: %s', res.data);
-                throw map403Error(res.data);
-            case 500:
-            case 503:
-                throw new Error('YouTube API is unavailable.  Please try again later.');
-            default:
-                if (res.statusCode !== 200) {
-                    throw new Error(`Error calling YouTube API: HTTP ${res.statusCode}`);
-                }
-                break;
-        }
-
-        let result;
-        try {
-            result = JSON.parse(res.data);
-        } catch (error) {
-            LOGGER.error(
-                'YouTube API returned non-JSON response: %s',
-                String(res.data).substring(0, 1000)
-            );
-            throw new Error('Error calling YouTube API: could not decode response as JSON');
-        }
-
+    return ytRequest(url, { headers }).then(res => {
         // Sadly, as of the v3 API, YouTube doesn't tell you *why* the request failed.
-        if (result.items.length === 0) {
+        if (res.items.length === 0) {
             throw new Error('Video does not exist or is private');
         }
 
-        const video = result.items[0];
-
-        if (!video.status || !video.contentDetails || !video.snippet) {
-            LOGGER.info('Incomplete video; assuming deleted video with id=%s', video.id);
-            throw new Error('This video is unavailable');
-        }
-
-        if (!video.status.embeddable) {
-            throw new Error('The uploader has made this video non-embeddable');
-        }
-
-        switch (video.status.uploadStatus) {
-            case 'deleted':
-                throw new Error('This video has been deleted');
-            case 'failed':
-                throw new Error(
-                    'This video is unavailable: ' +
-                    video.status.failureReason
-                );
-            case 'rejected':
-                throw new Error(
-                    'This video is unavailable: ' +
-                    video.status.rejectionReason
-                );
-            case 'processed':
-                break;
-            case 'uploaded':
-                // For VODs, we must wait for 'processed' before the video
-                // metadata is correct.  For livestreams, the status is
-                // 'uploaded' while the stream is live, and the metadata
-                // is presumably correct (we don't care about duration
-                // for livestreams anyways)
-                // See calzoneman/sync#710
-                //
-                // Sometimes, Youtube fails to set the status of a stream
-                // to live. However, we can check if 'actualStartTime' is
-                // present in the stream details to know it has started,
-                // and 'actualEndTime' to know it's ended.
-                if (video.snippet.liveBroadcastContent !== 'live' &&
-                    (!video.liveStreamingDetails?.actualStartTime || video.liveStreamingDetails?.actualEndTime)) {
-                    throw new Error(
-                        'This video has not been processed yet.'
-                    );
-                }
-                break;
-            default:
-                throw new Error(
-                    'This video is not available ' +
-                    `(status=${video.status.uploadStatus})`
-                );
-        }
-
-        const data = {
-            id,
-            type: 'youtube',
-            title: video.snippet.title,
-            duration: parseDuration(video.contentDetails.duration),
-            meta: {
-                thumbnail: video.snippet.thumbnails.default.url,
-                etag: result.etag
-            }
-        };
-
-        if (video.contentDetails.regionRestriction) {
-            const restriction = video.contentDetails.regionRestriction;
-            if (restriction.blocked) {
-                data.meta.blocked = restriction.blocked;
-            }
-            if (restriction.allowed) {
-                data.meta.allowed = restriction.allowed;
-            }
-        }
-
-        if (video.contentDetails.contentRating) {
-            const rating = video.contentDetails.contentRating;
-
-            if (rating.ytRating) {
-                data.meta.ytRating = rating.ytRating;
-            }
-        }
-
-        return new Media(data);
+        const video = res.items[0];
+        return videoToMedia(video, res.etag ?? null);
     });
+}
+
+function videoToMedia(video, etag = null) {
+    if (!video.status || !video.contentDetails || !video.snippet) {
+        LOGGER.info('Incomplete video; assuming deleted video with id=%s', video.id);
+        throw new Error('This video is unavailable');
+    }
+
+    if (!video.status.embeddable) {
+        throw new Error('The uploader has made this video non-embeddable');
+    }
+
+    switch (video.status.uploadStatus) {
+        case 'deleted':
+            throw new Error('This video has been deleted');
+        case 'failed':
+            throw new Error(
+                'This video is unavailable: ' +
+                video.status.failureReason
+            );
+        case 'rejected':
+            throw new Error(
+                'This video is unavailable: ' +
+                video.status.rejectionReason
+            );
+        case 'processed':
+            break;
+        case 'uploaded':
+            // For VODs, we must wait for 'processed' before the video
+            // metadata is correct.  For livestreams, the status is
+            // 'uploaded' while the stream is live, and the metadata
+            // is presumably correct (we don't care about duration
+            // for livestreams anyways)
+            // See calzoneman/sync#710
+            //
+            // Sometimes, Youtube fails to set the status of a stream
+            // to live. However, we can check if 'actualStartTime' is
+            // present in the stream details to know it has started,
+            // and 'actualEndTime' to know it's ended.
+            if (video.snippet.liveBroadcastContent !== 'live' &&
+                (!video.liveStreamingDetails?.actualStartTime || video.liveStreamingDetails?.actualEndTime)) {
+                throw new Error(
+                    'This video has not been processed yet.'
+                );
+            }
+            break;
+        default:
+            throw new Error(
+                'This video is not available ' +
+                `(status=${video.status.uploadStatus})`
+            );
+    }
+
+    const data = {
+        id: video.id,
+        type: 'youtube',
+        title: video.snippet.title,
+        duration: parseDuration(video.contentDetails.duration),
+        meta: {
+            thumbnail: video.snippet.thumbnails.default.url
+        }
+    };
+
+    if (etag !== null) {
+        data.meta.etag = etag;
+    }
+
+    if (video.contentDetails.regionRestriction) {
+        const restriction = video.contentDetails.regionRestriction;
+        if (restriction.blocked) {
+            data.meta.blocked = restriction.blocked;
+        }
+        if (restriction.allowed) {
+            data.meta.allowed = restriction.allowed;
+        }
+    }
+
+    if (video.contentDetails.contentRating) {
+        const rating = video.contentDetails.contentRating;
+
+        if (rating.ytRating) {
+            data.meta.ytRating = rating.ytRating;
+        }
+    }
+
+    return new Media(data);
 }
 
 /*
@@ -235,50 +249,31 @@ function _lookupInternal(id, cached) {
  *
  * Returns a list of Media objects
  */
-export function lookupMany(ids) {
+async function lookupMany(ids) {
     if (!API_KEY) {
-        return Promise.reject(new Error('API key not set for YouTube v3 API'));
+        throw new Error('API key not set for YouTube v3 API');
     }
 
     const url = new URL('https://www.googleapis.com/youtube/v3/videos');
     url.search = new URLSearchParams({
         key: API_KEY,
-        part: 'contentDetails,status,snippet',
+        part: 'contentDetails,status,snippet,liveStreamingDetails',
         id: ids.join(',')
     });
 
-    return getJSON(url).then(result => {
-        return result.items.filter(video => {
-            if (!video.status || !video.contentDetails || !video.snippet) {
-                LOGGER.info('Incomplete video; assuming deleted video with id=%s', video.id);
-                return false;
-            }
-
-            return video.status.embeddable
-        }).map(video => {
-            const data = {
-                id: video.id,
-                type: 'youtube',
-                title: video.snippet.title,
-                duration: parseDuration(video.contentDetails.duration),
-                meta: {
-                    thumbnail: video.snippet.thumbnails.default.url
-                }
-            };
-
-            if (video.contentDetails.regionRestriction) {
-                const restriction = video.contentDetails.regionRestriction;
-                if (restriction.blocked) {
-                    data.meta.blocked = restriction.blocked;
-                }
-                if (restriction.allowed) {
-                    data.meta.allowed = restriction.allowed;
-                }
-            }
-
-            return new Media(data);
-        });
+    const res = await ytRequest(url);
+    let results = [];
+    res.items.forEach(video => {
+        try {
+            results.push(videoToMedia(video));
+        } catch (error) {
+            // Skip -- likely a broken/deleted/non-embeddable video.  Let the
+            // rest of them be queued still.
+            LOGGER.info('Dropping video %s from playlist - %s', video.id, error.message);
+        }
     });
+
+    return results;
 }
 
 /*
@@ -318,33 +313,71 @@ export function search(query, nextPage = false) {
  *
  * Returns a list of Media objects
  */
-export function lookupPlaylist(id, nextPage = false) {
+export async function lookupPlaylist(id) {
     if (!API_KEY) {
-        return Promise.reject(new Error('API key not set for YouTube v3 API'));
+        throw new Error('API key not set for YouTube v3 API');
     }
 
+    LOGGER.info('Looking up YouTube playlist %s', id);
+
+    await checkPlaylistMeta(id);
+
     const url = new URL('https://www.googleapis.com/youtube/v3/playlistItems');
-    url.search = new URLSearchParams(Object.assign({
+    url.search = new URLSearchParams({
         key: API_KEY,
         part: 'contentDetails',
         maxResults: 50,
         playlistId: id
-    }, nextPage ? { pageToken: nextPage } : null));
-
-    return getJSON(url).then(result => {
-        return lookupMany(result.items.map(item => item.contentDetails.videoId))
-                .then(videos => {
-            if (result.nextPageToken) {
-                // Retrieve the rest of the results, then concatenate them with the current
-                // page
-                return lookupPlaylist(id, result.nextPageToken).then(
-                        other => videos.concat(other));
-            } else {
-                // No more pages of results
-                return videos;
-            }
-        });
     });
+    let results = [];
+    let nextPageToken = null;
+
+    do {
+        const res = await ytRequest(url);
+        const ids = res.items.map(it => it.contentDetails.videoId);
+        if (ids.length > 0) {
+            // Annoyingly, the playlistItems endpoint doesn't return the full
+            // contentDetails, so we have to go back to videos.list to get the
+            // info we need.
+            const videos = await lookupMany(ids);
+            results.push(...videos);
+        }
+
+        nextPageToken = res.nextPageToken ?? null;
+        if (nextPageToken !== null) {
+            LOGGER.info('Fetching next page of playlist %s, have %d items so far', id, results.length);
+            url.searchParams.set('pageToken', nextPageToken);
+        }
+    } while (nextPageToken !== null && results.length < PLAYLIST_ITEM_LIMIT);
+
+    if (nextPageToken !== null && results.length >= PLAYLIST_ITEM_LIMIT) {
+        LOGGER.warn('Length check failed for playlist %s: %d', id, results.length);
+        // Should have been checked already by checkPlaylistMeta, but YouTube's
+        // metadata cannot always be trusted.
+        throw new Error(`YouTube Playlist is too long to queue (limit ${PLAYLIST_ITEM_LIMIT}).`);
+    }
+
+    return results;
+}
+
+async function checkPlaylistMeta(id) {
+    const url = new URL('https://www.googleapis.com/youtube/v3/playlists');
+    url.search = new URLSearchParams({
+        key: API_KEY,
+        part: 'contentDetails',
+        id: id
+    });
+
+    const res = await ytRequest(url);
+    if (res.pageInfo.totalResults === 0) {
+        throw new Error('Invalid YouTube Playlist ID or unsupported playlist type');
+    }
+
+    const playlist = res.items[0];
+    if (playlist.contentDetails.itemCount > PLAYLIST_ITEM_LIMIT) {
+        LOGGER.warn('Rejecting YouTube Playlist %s for length %d', id, playlist.contentDetails.itemCount);
+        throw new Error(`YouTube Playlist is too long to queue (limit ${PLAYLIST_ITEM_LIMIT}).`);
+    }
 }
 
 /*
